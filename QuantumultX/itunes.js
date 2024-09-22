5076c152926c43ade656f761d9fcfbdc010f2502
Mike-offers <yrainyf777@gmail.com>
2024-09-22 11:35:31 +0800

    Update itunes.js

--- QuantumultX/itunes.js
+++ QuantumultX/itunes.js
@@ -52,6 +52,50 @@ var productidmap={
 	'dev.sanjin.WasteCat':['cat','dev.sanjin.WasteCat.PermanentVip',1]
 }
 
-// ========= 固定部分 ========= //
-ddgksf2013.Attention="恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";
-var mapid=ddgksf2013.receipt.bundle_id,mapping=productidmap[mapid]||productidmap.default,inapp={product_id:mapping[1],quantity:"1",expires_date:"2099-12-18 23:59:59 Etc/GMT",expires_date_pst:"2099-12-18 23:59:59 America/Los_Angeles",expires_date_ms:"4101292799000",is_in_intro_offer_period:"false",transaction_id:"100000000000000",is_trial_period:"false",original_transaction_id:"100000000000000",purchase_date_ms:"1701705599000",purchase_date:"2023-12-04 23:59:59 Etc/GMT",purchase_date_pst:"2023-12-04 23:59:59 America/Los_Angeles",original_purchase_date:"2023-12-04 23:59:59 Etc/GMT",original_purchase_date_pst:"2023-12-04 23:59:59 America/Los_Angeles",original_purchase_date_ms:"1701705599000",in_app_ownership_type:"PURCHASED",web_order_line_item_id:"100000000000000"},renew={product_id:mapping[1],original_transaction_id:"100000000000000",auto_renew_product_id:mapping[1],auto_renew_status:"1"};mapping[2]?(delete inapp.expires_date,delete inapp.expires_date_ms,delete inapp.expires_date_pst):(ddgksf2013.latest_receipt_info=[inapp],ddgksf2013.latest_receipt="https://t.me/ddgksf2021",ddgksf2013.pending_renewal_info=[renew]),ddgksf2013.receipt.in_app=[inapp],console.log('操作成功🎉🎉🎉\nCuttlefishの自留地: https://t.me/ddgksf2021'),$done({body:JSON.stringify(ddgksf2013)});
+// 固定部分
+ddgksf2013.Attention = "恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！";
+var mapid = ddgksf2013.receipt.bundle_id;
+var mapping = productidmap[mapid] || productidmap.default;
+
+var inapp = {
+    product_id: mapping[1],
+    quantity: "1",
+    expires_date: "2099-12-18 23:59:59 Etc/GMT",
+    expires_date_pst: "2099-12-18 23:59:59 America/Los_Angeles",
+    expires_date_ms: "4101292799000",
+    is_in_intro_offer_period: "false",
+    transaction_id: "100000000000000",
+    is_trial_period: "false",
+    original_transaction_id: "100000000000000",
+    purchase_date_ms: "1701705599000",
+    purchase_date: "2023-12-04 23:59:59 Etc/GMT",
+    purchase_date_pst: "2023-12-04 23:59:59 America/Los_Angeles",
+    original_purchase_date: "2023-12-04 23:59:59 Etc/GMT",
+    original_purchase_date_pst: "2023-12-04 23:59:59 America/Los_Angeles",
+    original_purchase_date_ms: "1701705599000",
+    in_app_ownership_type: "PURCHASED",
+    web_order_line_item_id: "100000000000000"
+};
+
+var renew = {
+    product_id: mapping[1],
+    original_transaction_id: "100000000000000",
+    auto_renew_product_id: mapping[1],
+    auto_renew_status: "1"
+};
+
+if (mapping[2]) {
+    delete inapp.expires_date;
+    delete inapp.expires_date_ms;
+    delete inapp.expires_date_pst;
+} else {
+    ddgksf2013.latest_receipt_info = [inapp];
+    ddgksf2013.latest_receipt = "https://t.me/ddgksf2021";
+    ddgksf2013.pending_renewal_info = [renew];
+}
+
+ddgksf2013.receipt.in_app = [inapp];
+
+console.log('操作成功🎉🎉🎉\nCuttlefishの自留地: https://t.me/ddgksf2021');
+
+$done({ body: JSON.stringify(ddgksf2013) });
