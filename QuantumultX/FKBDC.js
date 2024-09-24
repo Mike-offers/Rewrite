4a37870d644d59739d7cd306946ce75f2c76408e
Mike-offers <yrainyf777@gmail.com>
2024-09-24 22:48:57 +0800

    Update FKBDC.js

--- QuantumultX/FKBDC.js
+++ QuantumultX/FKBDC.js
@@ -1,14 +1,23 @@
 
+/*************************************
+
+>「 脚本名称 」         疯狂背单词解锁Vip
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-09-23
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，可能会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+**************************************
 
 [rewrite_local]
+https:\/\/api.fengkuangapp.com\/account\/grant\/detail\/info url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/FKBDC.js
 
-^http[s]?:\/\/api.fengkuangapp.com\/account\/grant\/detail\/info\/ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fengkuangapp.js
-
-[mitm] 
-
+[mitm]
 hostname = api.fengkuangapp.com
 
-*******************************/
+*************************************/
 
 var Mike = JSON.parse($response.body);
 
