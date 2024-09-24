99fc3eb817ea6bbe76da95ffce524b99f077b7ed
Mike-offers <yrainyf777@gmail.com>
2024-09-24 21:10:50 +0800

    Update Filmix.js

--- QuantumultX/Filmix.js
+++ QuantumultX/Filmix.js
@@ -1,26 +1,25 @@
+/*************************************
 
+>「 脚本名称 」         Filmix解锁Pro
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-09-24
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，可能会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
 
+**************************************
 
+[rewrite_local]
+https:\/\/appv2\.filmix\.com\.cn\/api\/v4\/users url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Filmix.js
 
+[mitm]
+hostname = appv2.filmix.com
 
+*************************************/
 
-
-
-let status = isJSON($response.body);
-let requestUrl = $request.url;
-var obj = status ? JSON.parse($response.body) : $response.body;
-if (/^https:\/\/appv2\.filmix\.com\.cn\/api\/v2\/users?/.test(requestUrl)) {
-  obj.is_vip = true;
-  obj.vip_start_time = "2024-04-02T10:10:10.101198+08:00";
-  obj.vip_end_time = "2222-02-02T15:33:15.101198+08:00";
-}
-$done({ body: status ? JSON.stringify(obj) : obj });
-
-function isJSON(str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
+var SuperMike = JSON.parse($response.body);
+Mike.is_vip = true;
+Mike.vip_start_time = "2009-09-09T10:10:10.101198+08:00";
+Mike.vip_end_time = "2099-09-09T15:33:15.101198+08:00";
+$done({ body: JSON.stringify(SuperMike)});
