009fa554c67b0a3b7b15e45637daf1c8ca5da972
Mike-offers <yrainyf777@gmail.com>
2024-09-25 23:06:45 +0800

    Create HLSEYY.js

---
+++ QuantumultX/HLSEYY.js
@@ -0,0 +1,19 @@
+/*************************************
+
+>「 脚本名称 」         呼啦少儿英语解锁Vip
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-09-24
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，可能会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/apiv2\.hula123\.com\/api\/v1\/me url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/HLSEYY.js
+
+[mitm]
+hostname = apiv2.hula123.com
+
+*************************************/
