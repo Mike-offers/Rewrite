ea874d41256eb73de106788ca4309a2ad4c87f7a
Mike-offers <yrainyf777@gmail.com>
2024-09-26 21:13:09 +0800

    Update Sorted.js

--- QuantumultX/Sorted.js
+++ QuantumultX/Sorted.js
@@ -8,8 +8,6 @@
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
 
-**************************************
-
 [rewrite_local]
 ^https?:\/\/api\.sortedapp\.com\/receipt$ url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Sorted.js
 
