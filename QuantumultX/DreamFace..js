2c90af00482b182a3a503f40c53d134daf10cf2e
Mike-offers <yrainyf777@gmail.com>
2024-10-02 20:15:07 +0800

    Create DreamFace..js

---
+++ QuantumultX/DreamFace..js
@@ -0,0 +1,17 @@
+/*************************************
+
+>「 脚本名称 」         DreamFace解锁Pro
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-09-26
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+[rewrite_local]
+^https?:\/\/dreamfaceapp\.com\/df-server\/user\/save_user_login url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/DreamFace.js
+
+[mitm]
+hostname = dreamfaceapp.com
+
+*************************************/
