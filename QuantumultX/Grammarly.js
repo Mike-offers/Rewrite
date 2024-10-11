019e0914f2e285b93da08af5902f8f21c7488e5c
Mike-offers <yrainyf777@gmail.com>
2024-10-11 13:00:30 +0800

    Update Grammarly.js

--- QuantumultX/Grammarly.js
+++ QuantumultX/Grammarly.js
@@ -1,6 +1,24 @@
-var objc = JSON.parse($response.body);
+/*************************************
 
-objc = {
+>「 脚本名称 」         Grammarly解锁Vip
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-09-26
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+[rewrite_local]
+^https?:\/\/.*\.grammarly\.com\/(api\/v\d\/subscription|v\d\/user\/oranonymous)  url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Grammarly.js
+
+[mitm]
+hostname = *.grammarly.com
+
+*************************************/
+
+var Mike = JSON.parse($response.body);
+
+Mike = {
   "isPremium": true,
   "nextPayDate": "Oct 28, 2999 4:56:52 AM",
   "isCancelled": false,
@@ -312,5 +330,5 @@ objc = {
 };
 
 $done({ 
-    JSON.stringify(objc); 
+    JSON.stringify(Mike); 
 });
