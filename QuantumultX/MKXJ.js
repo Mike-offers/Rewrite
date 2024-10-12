a4de239fad2acf4d48ecb6ad6c9b5831f344db9b
Mike-offers <yrainyf777@gmail.com>
2024-10-12 14:09:46 +0800

    Update MKXJ.js

--- QuantumultX/MKXJ.js
+++ QuantumultX/MKXJ.js
@@ -3,7 +3,7 @@
 >「 脚本名称 」         玛卡相机解锁Vip
 >「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
 >「 电报频道 」         https://t.me/TrollStoreKios 
->「 更新时间 」         2024-09-26
+>「 更新时间 」         2024-10-01
 >「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
@@ -18,5 +18,5 @@ hostname = *xdplt.com
 var body = $response.body.replace(/isVip":\d/g,'isVip":1')
 .replace(/vipExpireTime":\w+/g,'vipExpireTime":8887064968000')
 .replace(/isForeverVip":\d/g,'isForeverVip":1')
-.replace(/diamond":\d+/g,'diamond":888888')
+.replace(/diamond":\d+/g,'diamond":999999')
 $done({ body });
