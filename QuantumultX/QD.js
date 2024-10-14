263478c5788748c283b521e203ea08c80825fc40
Mike-offers <yrainyf777@gmail.com>
2024-10-14 23:02:55 +0800

    Update QD.js

--- QuantumultX/QD.js
+++ QuantumultX/QD.js
@@ -9,7 +9,7 @@
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-^https?:\/\/pc7dong.com\/seveng\/\/api\/user\/getUserById.v1? url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/QD.js
+^https?:\/\/pc7dong.com\/seveng.+api\/ url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/QD.js
 
 [mitm]
 hostname = pc7dong.com
