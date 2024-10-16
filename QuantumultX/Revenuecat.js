ea587f659fea11420df0310535d2570d58010e41
Mike-offers <yrainyf777@gmail.com>
2024-10-16 18:57:05 +0800

    Update Revenuecat.js

--- QuantumultX/Revenuecat.js
+++ QuantumultX/Revenuecat.js
@@ -11,14 +11,13 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
 
 [mitm]
-hostname = api.revenuecat.com
+hostname = api.revenuecat.com , isi.csan.goodnotes.com
 
 *************************************/
-
 
 const chxm1024 = {};
 const headers = $request.headers;
