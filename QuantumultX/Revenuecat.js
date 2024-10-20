1e92fda9ed9f37e885f7160015ee219171c106cd
Mike-offers <yrainyf777@gmail.com>
2024-10-20 12:44:38 +0800

    Update Revenuecat.js

--- QuantumultX/Revenuecat.js
+++ QuantumultX/Revenuecat.js
@@ -47,6 +47,7 @@ const bundle = {
 
 //识别UA
 const list = {
+  'AI%20Chat': { name: 'AI Plus', id: 'ai_plus_gpt_yearly', cm: 'sjb' },  //AI Chat - Assistant & Chatbot
   'Budget%20Flow': { name: 'full_access', id: 'com.fabian.hasse.haushaltsbuch.upgrade.combined', cm: 'sjb' },  //Budget Flow
   'Leaf%20VPN': { name: 'LeafPremium', id: 'com.kitslabs.production.Leaf.premium.monthlySub', cm: 'sja' },  //Leaf VPN
   'Unfold': { name: ["BESTDRESSED1_STORY",
