145144b9ddae30ba3ce8bba02a8f46ee5a1bfddf
Mike-offers <yrainyf777@gmail.com>
2024-11-05 18:02:02 +0800

    Update ZSBT.js

--- QuantumultX/ZSBT.js
+++ QuantumultX/ZSBT.js
@@ -9,11 +9,93 @@
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-^https?:\/\/w\.liupinyike\.com\/tablet\/api\/(vipIndex|card\/action) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/ZSBT.js
+^https?:\/\/w\.liupinyike\.com\/tablet\/api\/vipIndex url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/ZSBT.js
 
 [mitm]
 hostname = w.liupinyike.com
 
 *************************************/
 
-var _0xodm='\x6a\x73\x6a\x69\x61\x6d\x69\x2e\x63\x6f\x6d\x2e\x76\x37';const _0x196bdd=_0x3d1b;function _0x3d1b(_0x17f40a,_0x105418){const _0x5f3bb0=_0x5f3b();return _0x3d1b=function(_0x3d1b33,_0x1e9895){_0x3d1b33=_0x3d1b33-0xff;let _0x23f891=_0x5f3bb0[_0x3d1b33];if(_0x3d1b['ZgXBPB']===undefined){var _0x197e25=function(_0x9b578e){const _0x2d8ce0='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';let _0x1ce117='',_0x5f4922='';for(let _0x5bbfeb=0x0,_0xbfe30c,_0x33d043,_0x2f4a28=0x0;_0x33d043=_0x9b578e['charAt'](_0x2f4a28++);~_0x33d043&&(_0xbfe30c=_0x5bbfeb%0x4?_0xbfe30c*0x40+_0x33d043:_0x33d043,_0x5bbfeb++%0x4)?_0x1ce117+=String['fromCharCode'](0xff&_0xbfe30c>>(-0x2*_0x5bbfeb&0x6)):0x0){_0x33d043=_0x2d8ce0['indexOf'](_0x33d043);}for(let _0xee50d5=0x0,_0x5ad954=_0x1ce117['length'];_0xee50d5<_0x5ad954;_0xee50d5++){_0x5f4922+='%'+('00'+_0x1ce117['charCodeAt'](_0xee50d5)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x5f4922);};const _0x479dcb=function(_0x39e8a1,_0x18f59b){let _0x36224b=[],_0x5b6a4e=0x0,_0x39d264,_0x1681f7='';_0x39e8a1=_0x197e25(_0x39e8a1);let _0x13cd71;for(_0x13cd71=0x0;_0x13cd71<0x100;_0x13cd71++){_0x36224b[_0x13cd71]=_0x13cd71;}for(_0x13cd71=0x0;_0x13cd71<0x100;_0x13cd71++){_0x5b6a4e=(_0x5b6a4e+_0x36224b[_0x13cd71]+_0x18f59b['charCodeAt'](_0x13cd71%_0x18f59b['length']))%0x100,_0x39d264=_0x36224b[_0x13cd71],_0x36224b[_0x13cd71]=_0x36224b[_0x5b6a4e],_0x36224b[_0x5b6a4e]=_0x39d264;}_0x13cd71=0x0,_0x5b6a4e=0x0;for(let _0x4683c2=0x0;_0x4683c2<_0x39e8a1['length'];_0x4683c2++){_0x13cd71=(_0x13cd71+0x1)%0x100,_0x5b6a4e=(_0x5b6a4e+_0x36224b[_0x13cd71])%0x100,_0x39d264=_0x36224b[_0x13cd71],_0x36224b[_0x13cd71]=_0x36224b[_0x5b6a4e],_0x36224b[_0x5b6a4e]=_0x39d264,_0x1681f7+=String['fromCharCode'](_0x39e8a1['charCodeAt'](_0x4683c2)^_0x36224b[(_0x36224b[_0x13cd71]+_0x36224b[_0x5b6a4e])%0x100]);}return _0x1681f7;};_0x3d1b['ksbOqO']=_0x479dcb,_0x17f40a=arguments,_0x3d1b['ZgXBPB']=!![];}const _0x3418d8=_0x5f3bb0[0x0],_0xa84bff=_0x3d1b33+_0x3418d8,_0x1a3290=_0x17f40a[_0xa84bff];return!_0x1a3290?(_0x3d1b['whVcAf']===undefined&&(_0x3d1b['whVcAf']=!![]),_0x23f891=_0x3d1b['ksbOqO'](_0x23f891,_0x1e9895),_0x17f40a[_0xa84bff]=_0x23f891):_0x23f891=_0x1a3290,_0x23f891;},_0x3d1b(_0x17f40a,_0x105418);}(function(_0x47baf2,_0x262d51,_0xf02578,_0x913f4c,_0x52fefe,_0x18566d,_0x3a4f87){return _0x47baf2=_0x47baf2>>0x2,_0x18566d='hs',_0x3a4f87='hs',function(_0x58c097,_0x227d3e,_0x4f2c38,_0x4d57b1,_0xd51738){const _0x43fd6b=_0x3d1b;_0x4d57b1='tfi',_0x18566d=_0x4d57b1+_0x18566d,_0xd51738='up',_0x3a4f87+=_0xd51738,_0x18566d=_0x4f2c38(_0x18566d),_0x3a4f87=_0x4f2c38(_0x3a4f87),_0x4f2c38=0x0;const _0x1818bf=_0x58c097();while(!![]&&--_0x913f4c+_0x227d3e){try{_0x4d57b1=parseInt(_0x43fd6b(0x10f,'!Bgm'))/0x1*(parseInt(_0x43fd6b(0x110,'GUf['))/0x2)+parseInt(_0x43fd6b(0x10c,'hLlp'))/0x3+-parseInt(_0x43fd6b(0x114,'dEI9'))/0x4+-parseInt(_0x43fd6b(0x116,'HN^x'))/0x5+parseInt(_0x43fd6b(0x118,'G*fe'))/0x6*(parseInt(_0x43fd6b(0x117,'&C^C'))/0x7)+-parseInt(_0x43fd6b(0x11a,'4B$)'))/0x8+parseInt(_0x43fd6b(0x100,'Rvdd'))/0x9;}catch(_0x23a9b3){_0x4d57b1=_0x4f2c38;}finally{_0xd51738=_0x1818bf[_0x18566d]();if(_0x47baf2<=_0x913f4c)_0x4f2c38?_0x52fefe?_0x4d57b1=_0xd51738:_0x52fefe=_0xd51738:_0x4f2c38=_0xd51738;else{if(_0x4f2c38==_0x52fefe['replace'](/[EOhpBgRdbltVfNuYJG=]/g,'')){if(_0x4d57b1===_0x227d3e){_0x1818bf['un'+_0x18566d](_0xd51738);break;}_0x1818bf[_0x3a4f87](_0xd51738);}}}}}(_0xf02578,_0x262d51,function(_0x63514f,_0x225d8e,_0x226eea,_0x1d77da,_0x5b92e4,_0x4cc50f,_0x2df2f6){return _0x225d8e='\x73\x70\x6c\x69\x74',_0x63514f=arguments[0x0],_0x63514f=_0x63514f[_0x225d8e](''),_0x226eea='\x72\x65\x76\x65\x72\x73\x65',_0x63514f=_0x63514f[_0x226eea]('\x76'),_0x1d77da='\x6a\x6f\x69\x6e',(0x18a64e,_0x63514f[_0x1d77da](''));});}(0x32c,0xa0690,_0x5f3b,0xcd),_0x5f3b)&&(_0xodm=_0x5f3b);let Mike=JSON[_0x196bdd(0x109,'XD8q')]($response[_0x196bdd(0x10a,'w$V1')]),requestUrl=$request['url'];if(/^https?:\/\/w\.liupinyike\.com\/tablet\/api\/vipIndex?/[_0x196bdd(0x11e,'Rvdd')](requestUrl)){const LOhGik=_0x196bdd(0x113,'fHSt')[_0x196bdd(0x121,'JaRq')]('|');let PngepA=0x0;while(!![]){switch(LOhGik[PngepA++]){case'0':Mike[_0x196bdd(0xff,'0BwW')]['member_time']=0xf44b1925;continue;case'1':Mike[_0x196bdd(0x106,'JaRq')]['member_day']=0x270f;continue;case'2':Mike[_0x196bdd(0x10b,'TDW!')]['first_buy_vip']=0x1;continue;case'3':Mike[_0x196bdd(0x11c,'15I@')][_0x196bdd(0x104,'N&oW')]=0x1;continue;case'4':Mike[_0x196bdd(0x11f,'hLlp')][_0x196bdd(0x10e,'15I@')]=0x1;continue;case'5':Mike[_0x196bdd(0x120,'kXXv')][_0x196bdd(0x111,'fHSt')]=0x1;continue;}break;}}/https?:\/\/w\.liupinyike\.com\/tablet\/api\/card\/action?/[_0x196bdd(0x119,'JPt%')](requestUrl)&&(Mike[_0x196bdd(0x112,'kIra')][_0x196bdd(0x11d,'&PlH')]=0x270f,Mike['data'][_0x196bdd(0x101,'ICVY')]=0x1);$done({'body':JSON[_0x196bdd(0x103,'G*fe')](Mike)});function _0x5f3b(){const _0x467d9d=(function(){return[_0xodm,'YgEbjRtsljdiGadJpmiO.BucNoRhGmb.dvfVu7uO==','W59xW7JcVMqevtaWc8kj','uMHxAq','WOvyWPlcRJOwbHn0hSoj','W6RdQc4cftKAWOlcTSo3cmooWR4','WOuqWPhdQJjEbZuhd8k7W5BdKq','z3v2iZpdOeJcSCo2vmkADSkU','dmkBW4VcUCkHW7Pr','erpdLmkrfmovB8ovWPaTm8kvDG','W4JdGSoXWOi','ErNdOSk5W67cLGq0W6ZdTIeUWP8','mCkDht/dVafg','W4pdMNbD'].concat((function(){return['ghldMx/dJSk1lLTJWRHuwCkIW4i8nCojWO4u','wSkjsmk0','W6/dMmkBWRO','lhRdQmo7','yK/cKCoUWPi','W4lcK1TxW77dGwhcHSoWWP7cS8kUW74','FbpdPmk9W63cKWyVW5hdMXipWR4','WRRcGCk7rG','gSoydCoZW5hdP8oEihRcKxlcL8oX','W5COW4bQq8kHrCorWQZdOmkLWOFcMG','W4BdObyqWO3cVHO','v17cK8ojsmkcnmozWPS','itz9W7/dL8o9','wdtcJIxcLSoRC29qWRDRxSkd'].concat((function(){return['Dv7cICoM','WODWWOCGamomCCosWQldQmkAWQu','WOiqWPhdOtHDcX8+nSkZW4ZdKW','W69wp8krCG','Emklc8oJ','bSkxW5FcUq','WRJcJ8oDW6iXd8oUW7CeqmoRr8og','WPulWPRcJNy5WPhcTSoYr8oFW4zK','W47dIfTrWPZdT1eN','n8k7bZddUYXc','W6xdRW7dVttdKsCCo8kklXe'];}()));}()));}());_0x5f3b=function(){return _0x467d9d;};return _0x5f3b();};var version_ = '\x6a\x73\x6a\x69\x61\x6d\x69\x2e\x63\x6f\x6d\x2e\x76\x37';
+let Mike=JSON.parse($response.body);
+let requestUrl=$request.url;
+Mike = {
+  "status" : 0,
+  "msg" : "获取成功",
+  "data" : {
+    "is_vip" : 0,
+    "is_long_vip" : 0,
+    "vip_list" : [
+      {
+        "end_time" : null,
+        "vip_type" : 0,
+        "id" : 1,
+        "price" : "19.00",
+        "title" : "连续包月卡",
+        "start_time" : null,
+        "preferential_price" : "6.00",
+        "is_open" : 1,
+        "is_selected" : 2
+      },
+      {
+        "end_time" : "2024-06-21 23:59:59",
+        "vip_type" : 1,
+        "id" : 2,
+        "price" : "25.00",
+        "title" : "1个月·会员",
+        "start_time" : "2024-06-18 00:00:00",
+        "preferential_price" : "20.00",
+        "is_open" : 2,
+        "is_selected" : 2
+      },
+      {
+        "end_time" : "2024-06-21 23:59:59",
+        "vip_type" : 2,
+        "id" : 3,
+        "price" : "60.00",
+        "title" : "3个月·会员",
+        "start_time" : "2024-06-18 00:00:00",
+        "preferential_price" : "48.00",
+        "is_open" : 2,
+        "is_selected" : 2
+      },
+      {
+        "end_time" : "2024-06-21 23:59:59",
+        "vip_type" : 3,
+        "id" : 4,
+        "price" : "198.00",
+        "title" : "1年·会员",
+        "start_time" : "2024-06-18 00:00:00",
+        "preferential_price" : "158.00",
+        "is_open" : 2,
+        "is_selected" : 1
+      },
+      {
+        "end_time" : "2024-06-21 23:59:59",
+        "vip_type" : 100,
+        "id" : 6,
+        "price" : "698.00",
+        "title" : "永久·会员",
+        "start_time" : "2024-06-18 00:00:00",
+        "preferential_price" : "598.00",
+        "is_open" : 1,
+        "is_selected" : 2
+      },
+      {
+        "end_time" : "2024-06-21 23:59:59",
+        "vip_type" : 4,
+        "id" : 5,
+        "price" : "388.00",
+        "title" : "3年·会员",
+        "start_time" : "2024-06-18 00:00:00",
+        "preferential_price" : "328.00",
+        "is_open" : 2,
+        "is_selected" : 2
+      }
+    ],
+    "member_day" : 0,
+    "is_month" : 0,
+    "first_buy_vip" : 0,
+    "member_time" : 0
+  }
+};
+$done({body:JSON.stringify(Mike)});
