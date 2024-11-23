f88fe7d8bb9543f9a108917cfec69da8ac6b4189
Mike-offers <yrainyf777@gmail.com>
2024-11-23 22:34:41 +0800

    Add files via upload

---
+++ QuantumultX/WizardAI.js
@@ -0,0 +1,19 @@
+﻿﻿/*************************************
+
+>「 脚本名称 」         Wizard AI解锁Vip
+>「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
+>「 电报频道 」         https://t.me/TrollStoreKios 
+>「 更新时间 」         2024-11-05
+>「 注意事项 」         如需引用请注明出处，谢谢合作！
+>「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
+>「 额外说明 」         请勿传播或售卖此脚本
+
+[rewrite_local]
+^https?:\/\/europe-west1-ai-art-generator-services\.cloudfunctions\.net\/app\/user\/ url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/WizardAI.js
+
+[mitm]
+hostname = http://europe-west1-ai-art-generator-services.cloudfunctions.net
+
+*************************************/
+
+var _0xc2e=["","split","0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/","slice","indexOf","","",".","pow","reduce","reverse","0"];function _0xe85c(d,e,f){var g=_0xc2e[2][_0xc2e[1]](_0xc2e[0]);var h=g[_0xc2e[3]](0,e);var i=g[_0xc2e[3]](0,f);var j=d[_0xc2e[1]](_0xc2e[0])[_0xc2e[10]]()[_0xc2e[9]](function(a,b,c){if(h[_0xc2e[4]](b)!==-1)return a+=h[_0xc2e[4]](b)*(Math[_0xc2e[8]](e,c))},0);var k=_0xc2e[0];while(j>0){k=i[j%f]+k;j=(j-(j%f))/f}return k||_0xc2e[11]}eval(function(h,u,n,t,e,r){r="";for(var i=0,len=h.length;i<len;i++){var s="";while(h[i]!==n[e]){s+=h[i];i++}for(var j=0;j<n.length;j++)s=s.replace(new RegExp(n[j],"g"),j);r+=String.fromCharCode(_0xe85c(s,e,10)-t)}return decodeURIComponent(escape(r))}("kvvnkkvnkvHnHiTnkMvnkkunkMinkTinHiTnHMMnHiTnHuTnkivnkiHnkiinHkTnkMunkkvnkvHnkvknkTknHHMnHHinkvHnkTknkvknkMunkMvnkMMnkvknkTknHkTnkkunkMvnkTHnkuHnHHvnHMknMknkMvnkkunkMinkTinHiTnHMMnHiTnkuTnHiTnHivnkvknkvTnkkvnkvTnkvMnkvknHivnHMHnHiTnkvTnkvHnkvMnkTknHkHnHiTnHivnkTHnkkvnkvTnkkvnHivnHMHnHiTnkuTnHiTnHivnkvknkvMnkTinkTinkTknkvknkvknHivnHMHnHiTnkvTnkvHnkvMnkTknHkHnHiTnHivnkTinkvHnkTknkTHnkTunkvTnHvMnkkvnkvTnkkvnHivnHMHnHiTnkuTnHiTnHivnkkunkkvnkMknkkvnkMMnkTinkTknHivnHMHnHiTnHMinHMinHMinHMinHkHnHiTnHivnkTHnkkvnkTunkMknkuHnHvknkkvnkMknkkvnkMMnkTinkTknHivnHMHnHiTnHMinHMinHMinHMinHkHnHiTnHivnkMknkkvnkvknkvTnHvMnkkvnkTunkMknkuHnHvTnkTvnkTknkTinkMHnHivnHMHnHiTnHivnHTinHkvnHMinHMinHkknHkunHTinHkknHTHnHkunkiunHTinHTHnHMHnHTTnHMinHMHnHTTnHMinHkTnHMinHMinHMinkHvnHivnHkHnHiTnHivnkMknkkvnkvknkvTnkHknkTknkTknkMHnkMknkuHnHvTnkTvnkTknkTinkMHnHivnHMHnHiTnHivnHTinHkvnHMinHMinHkknHkunHTinHkknHTHnHkunkiunHTinHTHnHMHnHTTnHMinHMHnHTTnHMinHkTnHMinHMinHMinkHvnHivnHkHnHiTnHivnkvknkvMnkkunkvknkTinkvHnkTunkMunkvTnkTunkMvnkMMnkiunkuHnkMunkTknHivnHMHnHiTnHivnkiknkiMnkiHnHivnHkHnHiTnHivnkvknkvMnkkunkvknkTinkvHnkTunkMunkvTnkTunkMvnkMMnHvvnkMMnkTHnHvMnkkvnkvTnkTknHivnHMHnHiTnHivnHTinHkvnHMinHMinHkknHkunHTinHkknHTHnHkunkiunHTinHTHnHMHnHTTnHMinHMHnHTTnHMinHkTnHMinHMinHMinkHvnHivnHkHnHiTnHivnkTunkvknkiknkvHnkMvnHvHnkTinkvTnkTunkvvnkTknHivnHMHnHiTnkvTnkvHnkvMnkTknHkHnHiTnHivnkTunkvknHvHnkTunkivnkvTnkMvnkvHnkTknHvHnkTinkvTnkTunkvvnkTknHivnHMHnHiTnkvTnkvHnkvMnkTknHkHnHiTnHivnkkvnkTunkivnkvTnkMvnkvHnkTknHvvnkMMnkTHnHvMnkkvnkvTnkTknHivnHMHnHiTnHivnHTinHkvnHMinHMinHkknHkunHTinHkknHTHnHkunkiunHTinHTHnHMHnHTTnHMinHMHnHTTnHMinHkTnHMinHMinHMinkHvnHivnHiTnkuvnHkHnHiTnHivnkkvnkTHnkTHnkTknkTHnHvTnkvHnkTknkTHnkTunkvTnkvknHivnHMHnHiTnHMinHMinHMinHMinHiTnkuvnHkHnHiTnHivnkMTnkTknkvknkvknkkvnkTMnkTknHivnHMHnHiTnkMMnkvMnkMknkMknHkHnHiTnHivnkvTnkTunkMTnkTknkvknkvTnkkvnkMTnkMunHivnHMHnHiTnHivnHTinHkvnHMinHMinHkknHkunHTinHkknHTHnHkunkiunHTinHTHnHMHnHTTnHMinHMHnHTTnHMinHkTnHMinHMinHMinkHvnHivnMknkuvnHMknMknHHinkTHnkMvnkMMnkTknHHMnkuTnkkunkMvnkTHnkuHnHMHnHiTnHuTnkivnkiHnkiinHkTnkvknkvTnkvHnkTunkMMnkTMnkTunkTTnkuHnHHMnkMvnkkunkMinkTinHHvnkuvnHHvnHMkn",24,"iHkTMvuny",20,7,12))
