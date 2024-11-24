8b85cfb26d230e77a4dffbb3e6b890e03a7f6ef2
Mike-offers <yrainyf777@gmail.com>
2024-11-24 21:04:16 +0800

    Update Itunes.js

--- QuantumultX/Itunes.js
+++ QuantumultX/Itunes.js
@@ -15,7 +15,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/(verifyReceipt|analytics\/profile|receipt\/validate|verify|purchase)$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
