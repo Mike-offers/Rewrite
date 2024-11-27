8593afc89313a358cbd19e3adfdbada48e228739
Mike-offers <yrainyf777@gmail.com>
2024-11-27 20:35:26 +0800

    Update Revenuecat.js

--- QuantumultX/Revenuecat.js
+++ QuantumultX/Revenuecat.js
@@ -11,8 +11,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Revenuecat.js
-^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Revenuecat.js
+^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$|offers$) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Revenuecat.js
+^https:\/\/(api\.revenuecat|isi\.csan\.goodnotes)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$|offers$) url script-request-header https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Revenuecat.js
 
 [mitm]
 hostname = api.revenuecat.com , isi.csan.goodnotes.com
