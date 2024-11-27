a40ebeb0823cb33fc83796eeb84d1d2fe3881ca2
Mike-offers <yrainyf777@gmail.com>
2024-11-28 00:00:38 +0800

    Update Itunes.js

--- QuantumultX/Itunes.js
+++ QuantumultX/Itunes.js
@@ -31,6 +31,7 @@ const yearlysubscription = `${bundle_id}.yearlysubscription`;
 const lifetimeid = `${bundle_id}.lifetime`;
 
 const list = {
+  'in.muditbhargava.LookUp': { cm: 'timea', hx: 'hxpda', id: "lookup+.hasSubscribed", latest: "chxm1023" },  //LookUp
   'com.cuilingshi.fileextract': { cm: 'timea', hx: 'hxpda', id: "Unzip_ProVersion", latest: "chxm1023" },  //Unzip-解压缩
   'AIAssistant': { cm: 'timea', hx: 'hxpda', id: "AIchat_1w_7.99_trial", latest: "chxm1023" },  //AIAssistant
   'com.highonemob.weather': { cm: 'timeb', hx: 'hxpda', id: "com.highonemob.weather.base.w", latest: "chxm1023" },  //安心天气
