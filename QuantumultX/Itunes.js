28fc2c8aa16dacd8bbb2b52df0396fd8a0f8d6ca
Mike-offers <yrainyf777@gmail.com>
2024-12-14 02:31:34 +0800

    Update Itunes.js

--- QuantumultX/Itunes.js
+++ QuantumultX/Itunes.js
@@ -31,6 +31,7 @@ const yearlysubscription = `${bundle_id}.yearlysubscription`;
 const lifetimeid = `${bundle_id}.lifetime`;
 
 const list = {
+  'com.thirteenleafclover.art.generator': { cm: 'timeb', hx: 'hxpda', id: "com.thirteenleafclover.art.generator.unidreamlifetimevip", latest: "chxm1023" },  //Uni Dream
   'com.cuilingshi.flipclock': { cm: 'timeb', hx: 'hxpda', id: "FlipClockProVersion", latest: "chxm1023" },  //翻页时钟
   'com.weather.NOBWeather5': { cm: 'timea', hx: 'hxpda', id: "NOBWeather5.com.vip.yearly", latest: "chxm1023" },  //奈斯天气
   'com.maine.aifill': { cm: 'timeb', hx: 'hxpda', id: "com.maine.aifill.unlimited", latest: "chxm1023" },  //AI FILL-智能填充.换衣/换背景
