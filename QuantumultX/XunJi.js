bfb4ea55da35330e040186b50fe3cb5693c83cf8
Mike-offers <yrainyf777@gmail.com>
2024-12-25 16:45:33 +0800

    Add files via upload

--- QuantumultX/XunJi.js
+++ QuantumultX/XunJi.js
@@ -3,13 +3,13 @@
 >「 脚本名称 」         训记解锁Vip
 >「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
 >「 电报频道 」         https://t.me/TrollStoreKios 
->「 更新时间 」         2024-11-15
+>「 更新时间 」         2024-11-16
 >「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-^^https:\/\/api\.xunjiapp\.cn\/whole_user_info url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/XunJi.js
+^https?:\/\/api\.xunjiapp\.cn\/whole_user_info url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/XunJi.js
 
 [mitm]
 hostname = api.xunjiapp.cn
