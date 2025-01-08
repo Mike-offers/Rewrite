a6b13863db03d0f611d937693f5ec474b4b0036e
Mike-offers <yrainyf777@gmail.com>
2025-01-08 12:39:06 +0800

    Update CaiYunWeather.js

--- QuantumultX/CaiYunWeather.js
+++ QuantumultX/CaiYunWeather.js
@@ -32,7 +32,7 @@ const headers = Object.fromEntries(Object.entries($request.headers).map(([k, v])
 if (typeof $response == "undefined") {
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
   Mike.headers = { ...headers, 'device-token': token };
-  if (headers['app-version'] > '7.19.0') { chxm1024.headers['authorization'] = `Bearer ${token}`; }
+  if (headers['app-version'] > '7.19.0') { Mike.headers['authorization'] = `Bearer ${token}`; }
 } else {
   const data = { "is_auto_renewal": true, "expires_time": 4092599349 };
   //净化广告
