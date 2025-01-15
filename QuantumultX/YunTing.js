0dcbb579c50658a62a2366ebe462743f794f4a5c
Mike-offers <yrainyf777@gmail.com>
2025-01-15 17:33:17 +0800

    Update and rename YuTing.js to YunTing.js

--- QuantumultX/YunTing.js
+++ QuantumultX/YunTing.js
@@ -9,7 +9,7 @@
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-(^https?:\/\/(ytmsout|ytapi|getway)\.radio\.cn|60\.205\.171\.165)\/(contentBiz|publish|rights|(user|ytsrv\/srv)\/(appUser|webPage)\/getUserInfo|app.+) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/YuTing.js
+(^https?:\/\/(ytmsout|ytapi|getway)\.radio\.cn|60\.205\.171\.165)\/(contentBiz|publish|rights|(user|ytsrv\/srv)\/(appUser|webPage)\/getUserInfo|app.+) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/YunTing.js
 
 [mitm]
 hostname = *.radio.cn, 60.205.171.165
