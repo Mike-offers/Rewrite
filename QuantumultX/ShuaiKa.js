c9937a505acc0eacc66c1c1cfe05d5404d841206
Mike-offers <yrainyf777@gmail.com>
2025-01-23 14:40:34 +0800

    Update ShuaiKa.js

--- QuantumultX/ShuaiKa.js
+++ QuantumultX/ShuaiKa.js
@@ -6,7 +6,7 @@
 >「 更新时间 」         2025-01-22
 >「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
->「 额外说明 」         此为引用脚本的解密脚本，请勿传播或售卖此脚本
+>「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
 ^https?:\/\/api\.shuaika\.com\/api\/v\d\/vip$ url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/ShuaiKa.js
