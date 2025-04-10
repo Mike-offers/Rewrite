f75a232ba9a7f77e8e0bd386e8efde4ef867e231
Mike-offers <yrainyf777@gmail.com>
2025-04-11 01:13:24 +0800

    Update Aemulo.js

--- QuantumultX/Aemulo.js
+++ QuantumultX/Aemulo.js
@@ -9,7 +9,7 @@
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-^https?:\/\/api\.anamy\.xyz\/public\/aemulo\/purchased url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/1998KT.js
+^https?:\/\/api\.anamy\.xyz\/public\/aemulo\/purchased url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Aemulojs
 
 [mitm]
 hostname = api.anamy.xyz
