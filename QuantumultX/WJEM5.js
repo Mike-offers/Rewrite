80e52cc855557f3c7f7442a317f1dc90de0c9f5b
Mike-offers <yrainyf777@gmail.com>
2025-05-04 21:07:51 +0800

    Add files via upload

--- QuantumultX/WJEM5.js
+++ QuantumultX/WJEM5.js
@@ -1,18 +1,18 @@
 ﻿/*************************************
 
->「 脚本名称 」         解除XPTV闪退限制
+>「 脚本名称 」         解锁无尽噩梦 5 咒怨无限资源
 >「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
 >「 电报频道 」         https://t.me/TrollStoreKios 
->「 更新时间 」         2025-04-17
+>「 更新时间 」         2025-05-04
 >「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 >「 额外说明 」         请勿传播或售卖此脚本
 
 [rewrite_local]
-^https?:\/\/infuseku\.xyz url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/XPTV.js
+^http[s]?:\/\/kb\.chenguanserver\.com\/5\/game\/api\/v1\/activity\/(config|redeem|com) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/WJEM5.js
 
 [mitm]
-hostname = infuseku.xyz
+hostname = kb.chenguanserver.com
 
 *************************************/
 
